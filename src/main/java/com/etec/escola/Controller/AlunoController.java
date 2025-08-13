92be5d4962b239bf3bb61e664d1e07543d8ae27f
Eduardo Araujo <eduaraujopires370@gmail.com>
2025-08-13 17:22:15 -0300

    feat: Classe controle

--- src/main/java/com/etec/escola/Controller/AlunoController.java
+++ src/main/java/com/etec/escola/Controller/AlunoController.java
@@ -1,4 +1,11 @@
 package com.etec.escola.Controller;
 
+import com.etec.escola.Service.Aluno_Service;
+
 public class AlunoController {
+    private final Aluno_Service  alunoService;
+
+    public AlunoController(Aluno_Service alunoService) {
+        this.alunoService = alunoService;
+    }
 }
